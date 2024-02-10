9878181b03a6d761e52f8ec01c400ad426fd4d6f
lugh-tuatha <aceonsteam001@gmail.com>
2024-02-11 03:35:11 +0800

    setup: project archi, style: homepage hero, responsiveness of hero

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-			'primary': '#FFEDC7',
-			'accent': '#93B981',
+		extend: {
+			colors: {
+				'primary': '#FFEDC7',
+				'accent': '#93B981',
+			},
 		},
-		extend: {},
 	},
 	plugins: [],
 }
