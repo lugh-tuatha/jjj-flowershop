e33b0871d1d81a5bab86b1b11fcf21e6970f00e7
lugh-tuatha <aceonsteam001@gmail.com>
2024-02-10 15:23:58 +0800

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'primary': '#FFEDC7',
+			'accent': '#93B981',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
